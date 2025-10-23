7c4fad3eada45db80926eb38ecf992d6f7f95662
pranab <pranabm406@gmail.com>
2025-10-23 21:25:10 +0530

    user-schema

--- models/user.models.js
+++ models/user.models.js
@@ -1,8 +1,16 @@
-import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
+import {timestamp, pgTable, varchar, uuid, text } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
+
+  firstname: varchar("first_name",{ length: 55 }).notNull(),
+  lastname: varchar("last_name",{ length: 55 }),
+
   email: varchar({ length: 255 }).notNull().unique(),
+
+  password: text().notNull(),
+  salt: text().notNull(),
+
+  createdAT: timestamp("created_at").notNull().defaultNow(),
+  updatedAT: timestamp("updated_at").$onUpdate(() => new Date()),
 });
