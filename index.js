b3c7002252834a8ecf0b41968b58f57309fcc433
pranab <pranabm406@gmail.com>
2025-10-24 02:15:18 +0530

    signup routes

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
+import signup from "./routes/user.routes.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ status: "server is up and running" });
 });
+
+app.use("/user", signup);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
